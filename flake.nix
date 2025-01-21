5044d1019247dbb014dbc6dda020ffa1eed6a09c
ravi-sawlani-yral <ravi@gobazzinga.io>
2025-01-21 20:25:07 +0530

    update dependency

--- flake.nix
+++ flake.nix
@@ -13,10 +13,25 @@
     {
       devShells.${system}.default = pkgs.mkShell
       {
-        packages = with pkgs; [ rustup cargo ]++ (if pkgs.stdenv.isDarwin then [
+        packages = with pkgs; [ rustup cargo cargo-leptos ]++ (if pkgs.stdenv.isDarwin then [
             darwin.apple_sdk.frameworks.Foundation
             pkgs.darwin.libiconv
+            tailwindcss
+            git
           ] else []);
+          shellHook = ''
+            if [ -d "/opt/homebrew/opt/llvm" ]; then
+              export LLVM_PATH="/opt/homebrew/opt/llvm"
+            else
+              export LLVM_PATH="$(which llvm)"
+            fi
+            export RUSTC_WRAPPER=""
+            export CC_wasm32_unknown_unknown=$LLVM_PATH/bin/clang
+            export CXX_wasm32_unknown_unknown=$LLVM_PATH/bin/clang++
+            export AS_wasm32_unknown_unknown=$LLVM_PATH/bin/llvm-as
+            export AR_wasm32_unknown_unknown=$LLVM_PATH/bin/llvm-ar
+            export STRIP_wasm32_unknown_unknown=$LLVM_PATH/bin/llvm-strip
+          '';
       };
     };
 }
